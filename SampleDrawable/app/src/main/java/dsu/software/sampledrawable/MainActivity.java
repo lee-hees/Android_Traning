046d4ad76b9d6e38e4cdab234cb68c80ce376b18
lee-hees <2sdsf00@naver.com>
2020-04-13 22:53:27 +0900

    Create SampleDrawable

---
+++ SampleDrawable/app/src/main/java/dsu/software/sampledrawable/MainActivity.java
@@ -0,0 +1,14 @@
+package dsu.software.sampledrawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
