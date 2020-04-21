dedd4f682d58eef13f2285017fc4cc174de83ef8
lee-hees <2sdsf00@naver.com>
2020-04-21 22:53:36 +0900

    Create SampleOrientation - vertical / horizontal

---
+++ SampleOrientation/app/src/main/java/dsu/software/sampleorientation/MainActivity.java
@@ -0,0 +1,64 @@
+package dsu.software.sampleorientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String name;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showToast("onCreate 호출됨");
+
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "입력된 값을 변수에 저장했습니다 : " + name, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if (savedInstanceState != null) {
+            name = savedInstanceState.getString("name");
+            Toast.makeText(this, "값을 복원했습니다 : " + name, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putString("name",name);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart 호출됨");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop 호출됨");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy 호출됨");
+    }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+    }
+}
