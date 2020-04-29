c5e934d8738ae861dd137c4dcb70320c6975d5f8
lee-hees <2sdsf00@naver.com>
2020-04-29 19:09:44 +0900

    Create SampleLifeCycle

---
+++ SampleLifeCycle/app/src/main/java/dsu/software/samplelifecycle/MenuActivity.java
@@ -0,0 +1,24 @@
+package dsu.software.samplelifecycle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
