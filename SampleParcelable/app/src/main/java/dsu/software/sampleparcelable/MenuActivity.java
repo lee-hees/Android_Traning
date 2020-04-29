982c118e58b3a144a6fcab51edc5b60e9bec8873
lee-hees <2sdsf00@naver.com>
2020-04-29 17:58:38 +0900

    Create SampleParcelable

---
+++ SampleParcelable/app/src/main/java/dsu/software/sampleparcelable/MenuActivity.java
@@ -0,0 +1,48 @@
+package dsu.software.sampleparcelable;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    TextView textView;
+    public static final String KEY_SIMPLE_DATA = "data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "data");
+                setResult(RESULT_OK, intent);
+
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        processIntent(intent);
+    }
+
+    private void processIntent(Intent intent) {
+        if (intent != null) {
+            Bundle bundle = intent.getExtras();
+            SimpleData data = bundle.getParcelable(KEY_SIMPLE_DATA);
+            if (data != null) {
+                textView.setText("전달 받은 데이터\nNumber : " + data.number
+                        + "\nMessage : " + data.message);
+            }
+        }
+    }
+}
