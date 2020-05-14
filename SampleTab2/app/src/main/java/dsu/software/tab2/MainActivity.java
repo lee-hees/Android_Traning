b1d0bb2ea4965cd475990382d125fb38ad01c772
lee-hees <2sdsf00@naver.com>
2020-05-14 22:40:57 +0900

    SampleTab2

---
+++ SampleTab2/app/src/main/java/dsu/software/tab2/MainActivity.java
@@ -0,0 +1,54 @@
+package dsu.software.tab2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private Fragment1 fragmentSearch = new Fragment1();
+    private Fragment2 fragmentCamera = new Fragment2();
+    private Fragment3 fragmentCall = new Fragment3();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.searchItem:
+                    transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
+
+                    break;
+                case R.id.cameraItem:
+                    transaction.replace(R.id.frameLayout, fragmentCamera).commitAllowingStateLoss();
+                    break;
+                case R.id.callItem:
+                    transaction.replace(R.id.frameLayout, fragmentCall).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
+}
