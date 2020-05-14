6e506cdb9a0863fa1a5609e422ef7283d2919c87
lee-hees <2sdsf00@naver.com>
2020-05-14 17:29:01 +0900

    SampleActionBar2

---
+++ SampleActionBar2/app/src/main/java/dsu/software/actionbar2/MainActivity.java
@@ -0,0 +1,43 @@
+package dsu.software.actionbar2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        View v = menu.findItem(R.id.menu_search).getActionView();
+
+        if (v != null) {
+            EditText editText = v.findViewById(R.id.editText);
+
+            if (editText != null) {
+                editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                        Toast.makeText(getApplicationContext(), "입력됨", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+            }
+        }
+
+        return true;
+    }
+}
