56755e155fd461c4f8b3d0f29e0203e6f5a817c0
lee-hees <2sdsf00@naver.com>
2020-06-03 15:33:03 +0900

    SampleTweenAnimation

---
+++ SampleTweenAnimation/app/src/main/java/dsu/software/sampletweenanimation/MainActivity.java
@@ -0,0 +1,63 @@
+package dsu.software.sampletweenanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale1);
+                view.startAnimation(anim);
+            }
+        });
+
+        Button btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale2);
+                view.startAnimation(anim);
+            }
+        });
+
+        Button translate = findViewById(R.id.translate);
+        translate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate);
+                view.startAnimation(anim);
+            }
+        });
+
+        Button rotate = findViewById(R.id.rotate);
+        rotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+                view.startAnimation(anim);
+            }
+        });
+
+        Button alpha = findViewById(R.id.alpha);
+        alpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.alpha);
+                view.startAnimation(anim);
+            }
+        });
+    }
+}
