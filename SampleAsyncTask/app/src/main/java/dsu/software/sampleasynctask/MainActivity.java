14eb4fb637a38d72cbfddacf33a86346f8020fde
lee-hees <2sdsf00@naver.com>
2020-06-11 23:47:21 +0900

    SampleAsyncTask

---
+++ SampleAsyncTask/app/src/main/java/dsu/software/sampleasynctask/MainActivity.java
@@ -0,0 +1,82 @@
+package dsu.software.sampleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    BackgroundTask task;
+    int value;
+
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressbar);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task = new BackgroundTask();
+                task.execute();
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.cancel(true);
+            }
+        });
+    }
+
+    class BackgroundTask extends AsyncTask<Integer, Integer, Integer> {
+        @Override
+        protected void onPreExecute() {
+            value = 0;
+            progressBar.setProgress(value);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            while (isCancelled() == false) {
+                value++;
+                if (value >= 100) {
+                    break;
+                } else {
+                    publishProgress(value);
+                }
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                }
+            }
+            return value;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0].intValue());
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            progressBar.setProgress(0);
+        }
+
+        @Override
+        protected void onCancelled() {
+            progressBar.setProgress(0);
+        }
+    }
+}
